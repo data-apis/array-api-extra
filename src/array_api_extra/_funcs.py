7ac8e8f3e8e08f5a0edfef6fa78011127eb27ec1
Lucas Colley <lucas.colley8@gmail.com>
2024-09-20 12:30:15 +0100

    API: remove dependency on array-api-compat

--- src/array_api_extra/_funcs.py
+++ src/array_api_extra/_funcs.py
@@ -2,15 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from array_api_compat import array_namespace  # type: ignore[import-not-found]
-
 if TYPE_CHECKING:
     from ._typing import Array, ModuleType
 
 __all__ = ["atleast_nd"]
 
 
-def atleast_nd(x: Array, *, ndim: int, xp: ModuleType | None = None) -> Array:
+def atleast_nd(x: Array, *, ndim: int, xp: ModuleType) -> Array:
     """
     Recursively expand the dimension of an array to have at least `ndim`.
 
@@ -19,6 +17,12 @@ def atleast_nd(x: Array, *, ndim: int, xp: ModuleType | None = None) -> Array:
     x: array
         An array.
 
+    ndim: int
+        The minimum number of dimensions for the result.
+
+    xp: array_namespace
+        The array namespace for `x`.
+
     Returns
     -------
     res: array
@@ -26,8 +30,6 @@ def atleast_nd(x: Array, *, ndim: int, xp: ModuleType | None = None) -> Array:
         If ``x.ndim`` >= `ndim`, `x` is returned.
         If ``x.ndim`` < `ndim`, ``res.ndim`` will equal `ndim`.
     """
-    xp = array_namespace(x) if xp is None else xp
-
     x = xp.asarray(x)
     if x.ndim < ndim:
         x = xp.expand_dims(x, axis=0)
