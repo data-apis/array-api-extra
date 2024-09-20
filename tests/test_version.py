6fff003466d40622f8b20bf7f1f3c06697f67d10
Lucas Colley <lucas.colley8@gmail.com>
2024-09-20 10:59:44 +0100

    DEV: set up package with cookie (#1)

---
+++ tests/test_version.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+import importlib.metadata
+
+import array_api_extra as xpx
+
+
+def test_version():
+    assert importlib.metadata.version("array_api_extra") == xpx.__version__
