7ce7e2a20e4e1e885c5b921cb86a56d3f8ee4194
Lucas Colley <lucas.colley8@gmail.com>
2024-09-20 18:43:20 +0100

    TST: `atleast_nd`: full tests

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1,12 +1,68 @@
 from __future__ import annotations
 
-import array_api_strict as xp  # type: ignore[import-not-found]
+import array_api_strict as xp
+from numpy.testing import assert_array_equal
 
 from array_api_extra import atleast_nd
 
 
 class TestAtLeastND:
-    def test_1d_to_2d(self):
+    def test_0D(self):
+        x = xp.asarray(1)
+
+        y = atleast_nd(x, ndim=0, xp=xp)
+        assert_array_equal(y, x)
+
+        y = atleast_nd(x, ndim=1, xp=xp)
+        assert_array_equal(y, xp.ones((1,)))
+
+        y = atleast_nd(x, ndim=5, xp=xp)
+        assert_array_equal(y, xp.ones((1, 1, 1, 1, 1)))
+
+    def test_1D(self):
         x = xp.asarray([0, 1])
+
+        y = atleast_nd(x, ndim=0, xp=xp)
+        assert_array_equal(y, x)
+
+        y = atleast_nd(x, ndim=1, xp=xp)
+        assert_array_equal(y, x)
+
         y = atleast_nd(x, ndim=2, xp=xp)
-        assert y.ndim == 2
+        assert_array_equal(y, xp.asarray([[0, 1]]))
+
+        y = atleast_nd(x, ndim=5, xp=xp)
+        assert_array_equal(y, xp.reshape(xp.arange(2), (1, 1, 1, 1, 2)))
+
+    def test_2D(self):
+        x = xp.asarray([[3]])
+
+        y = atleast_nd(x, ndim=0, xp=xp)
+        assert_array_equal(y, x)
+
+        y = atleast_nd(x, ndim=2, xp=xp)
+        assert_array_equal(y, x)
+
+        y = atleast_nd(x, ndim=3, xp=xp)
+        assert_array_equal(y, 3 * xp.ones((1, 1, 1)))
+
+        y = atleast_nd(x, ndim=5, xp=xp)
+        assert_array_equal(y, 3 * xp.ones((1, 1, 1, 1, 1)))
+
+    def test_5D(self):
+        x = xp.ones((1, 1, 1, 1, 1))
+
+        y = atleast_nd(x, ndim=0, xp=xp)
+        assert_array_equal(y, x)
+
+        y = atleast_nd(x, ndim=4, xp=xp)
+        assert_array_equal(y, x)
+
+        y = atleast_nd(x, ndim=5, xp=xp)
+        assert_array_equal(y, x)
+
+        y = atleast_nd(x, ndim=6, xp=xp)
+        assert_array_equal(y, xp.ones((1, 1, 1, 1, 1, 1)))
+
+        y = atleast_nd(x, ndim=9, xp=xp)
+        assert_array_equal(y, xp.ones((1, 1, 1, 1, 1, 1, 1, 1, 1)))
