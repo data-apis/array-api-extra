3dad7f76b1786ceb5fc8a4ea811fe447ca36211d
Lucas Colley <lucas.colley8@gmail.com>
2024-11-15 14:44:59 +0000

    TYP: remove `TYPE_CHECKING` guard from `_typing`

--- src/array_api_extra/_typing.py
+++ src/array_api_extra/_typing.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
 
 from types import ModuleType
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
-if TYPE_CHECKING:
-    Array = Any  # To be changed to a Protocol later (see array-api#589)
+Array = Any  # To be changed to a Protocol later (see array-api#589)
 
 __all__ = ["Array", "ModuleType"]
