a5a6421416bc1e8cd015f917d92c28c99e144499
Lucas Colley <lucas.colley8@gmail.com>
2024-11-20 11:30:01 +0000

    DEV: set version to 0.2.0

--- src/array_api_extra/__init__.py
+++ src/array_api_extra/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from ._funcs import atleast_nd, cov, create_diagonal, expand_dims, kron, sinc
 
-__version__ = "0.1.2.dev0"
+__version__ = "0.2.0"
 
 __all__ = [
     "__version__",
