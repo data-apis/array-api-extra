5cc1140d7e8e226837ccada62ec4880294a6f9c1
Lucas Colley <lucas.colley8@gmail.com>
2024-11-29 23:37:36 +0000

    adjust test to hit alternative path

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,8 +10,8 @@ from array_api_extra._lib._utils import in1d
 # some test coverage already provided by TestSetDiff1D
 class TestIn1D:
     def test_no_invert_assume_unique(self):
-        x1 = xp.asarray([1, 2, 3])
-        x2 = xp.asarray([3, 4, 5])
-        expected = xp.asarray([False, False, True])
+        x1 = xp.asarray([3, 8, 20])
+        x2 = xp.arange(15)
+        expected = xp.asarray([True, True, False])
         actual = in1d(x1, x2, xp=xp)
         assert_array_equal(actual, expected)
