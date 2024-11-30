39076915e3c5b08a67fa46bfeca0386148c52301
Lucas Colley <lucas.colley8@gmail.com>
2024-11-30 02:25:13 +0000

    DEV: set version to 0.3.2.dev0

--- src/array_api_extra/__init__.py
+++ src/array_api_extra/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from ._funcs import atleast_nd, cov, create_diagonal, expand_dims, kron, setdiff1d, sinc
 
-__version__ = "0.3.1"
+__version__ = "0.3.2.dev0"
 
 # pylint: disable=duplicate-code
 __all__ = [
