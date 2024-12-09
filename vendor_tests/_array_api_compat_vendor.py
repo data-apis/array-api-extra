7046ee55957948d86a962550432acbfd627579f9
crusaderky <crusaderky@gmail.com>
2024-12-09 11:30:57 +0000

    Code review

--- vendor_tests/_array_api_compat_vendor.py
+++ vendor_tests/_array_api_compat_vendor.py
@@ -5,5 +5,7 @@ from .array_api_compat import *  # noqa: F403
 from .array_api_compat import array_namespace as array_namespace_compat
 
 
+# Let unit tests check with `is` that we are picking up the function from this module
+# and not from the original array_api_compat module.
 def array_namespace(*xs, **kwargs):
     return array_namespace_compat(*xs, **kwargs)
