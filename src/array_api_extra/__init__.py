e2ef56882b5e21f64c6eedd6fcaaf0c62ec7cd33
Lucas Colley <lucas.colley8@gmail.com>
2024-12-12 19:38:55 +0000

    REL: set version to 0.4.0

--- src/array_api_extra/__init__.py
+++ src/array_api_extra/__init__.py
@@ -1,6 +1,6 @@
 from ._funcs import atleast_nd, cov, create_diagonal, expand_dims, kron, setdiff1d, sinc
 
-__version__ = "0.3.3.dev0"
+__version__ = "0.4.0"
 
 # pylint: disable=duplicate-code
 __all__ = [
