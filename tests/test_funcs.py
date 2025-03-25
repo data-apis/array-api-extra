380a81cac12f19142297fc631fea21c5283c0cef
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2025-03-25 11:22:35 -0400

    fix pre-commit

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -720,7 +720,10 @@ class TestIsClose:
     def test_device(self, xp: ModuleType, device: Device, equal_nan: bool):
         a = xp.asarray([0.0, 0.0, xp.nan], device=device)
         b = xp.asarray([1e-9, 1e-4, xp.nan], device=device)
-        xp_assert_equal(isclose(a, b, equal_nan=equal_nan), xp.asarray([True, False, equal_nan]))
+        xp_assert_equal(
+            isclose(a, b, equal_nan=equal_nan), xp.asarray([True, False, equal_nan])
+        )
+
 
 class TestKron:
     def test_basic(self, xp: ModuleType):
