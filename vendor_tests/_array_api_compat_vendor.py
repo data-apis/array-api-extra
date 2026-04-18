af81672ceeb3cadc2b779b34b8ba9c34242740b7
Lucas Colley <lucas.colley8@gmail.com>
2026-04-18 16:59:51 +0100

    iter

--- vendor_tests/_array_api_compat_vendor.py
+++ vendor_tests/_array_api_compat_vendor.py
@@ -4,11 +4,11 @@
 from types import ModuleType
 from typing import Any
 
-from .array_api_compat import *  # noqa: F403 # pyright: ignore[reportAssignmentType]
+from .array_api_compat import *  # noqa: F403
 from .array_api_compat import array_namespace as array_namespace_compat
 
 
 # Let unit tests check with `is` that we are picking up the function from this module
 # and not from the original array_api_compat module.
 def array_namespace(*xs: Any | complex | None, **kwargs) -> ModuleType:  # type: ignore[no-redef] # numpydoc ignore=GL08
-    return array_namespace_compat(*xs, **kwargs)  # pyright: ignore[reportUnknownArgumentType]
+    return array_namespace_compat(*xs, **kwargs)
