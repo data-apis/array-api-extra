4ccaf4a0941fd6ecd0fb31b33854d6a6d44a60b1
Pradyot Ranjan <99216956+prady0t@users.noreply.github.com>
2026-05-12 01:23:57 +0530

    Update tests/test_funcs.py
    Co-authored-by: Lucas Colley <lucas.colley8@gmail.com>

--- tests/test_funcs.py
+++ tests/test_funcs.py
@@ -1944,7 +1944,7 @@ class TestAngle:
     def test_2d(self, xp: ModuleType):
         a = xp.asarray([[1 + 1j, 1 - 1j], [-1 + 1j, -1 - 1j]])
         expected = xp.asarray(
-            [[np.pi / 4, -np.pi / 4], [3 * np.pi / 4, -3 * np.pi / 4]]
+            [[xp.pi / 4, -xp.pi / 4], [3 * xp.pi / 4, -3 * xp.pi / 4]]
         )
         res = angle(a)
         xp_assert_equal(res, expected)
